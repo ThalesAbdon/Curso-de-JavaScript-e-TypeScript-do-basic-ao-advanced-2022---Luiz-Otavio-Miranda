867e6475ab7c049284f679d14d02da4508f36b76
Thales <thales.sousufpi@gmail.com>
2022-04-17 20:37:44 -0300

    Comentários em Javascript

---
+++ Módulo 02 - JavaScript Básico - Para iniciantes/Aula 03 - Comentários/comentarios.js
@@ -0,0 +1,20 @@
+//escrevendo comentário
+console.log("olá mundo"); //aqui tem outro comentário
+console.log('Linha 1');
+//Comentário de uma linha
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+console.log('Linha 5');
+/*Comentário para várias linhas
+linhas
+linha 
+linha 
+linha 
+linha 
+linha */
+
+console.log('Linha 2');
+//console.log('Linha 3');
+console.log('Linha 4');
+console.log('Linha 5');
