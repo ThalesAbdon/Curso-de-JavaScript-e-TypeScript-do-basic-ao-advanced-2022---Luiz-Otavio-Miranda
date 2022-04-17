cf86afe95971142aa9e1f3ca2620c4cd0a2422e7
Thales <thales.sousufpi@gmail.com>
2022-04-17 20:30:59 -0300

    Aprendendo e treinando console.log()

--- Módulo 02 - JavaScript Básico - Para iniciantes/Aula 01  - Console.log/index.js
+++ Módulo 02 - JavaScript Básico - Para iniciantes/Aula 01  - Console.log/index.js
@@ -3,4 +3,4 @@ console.log("'Oi, testando o console.log'"); //String
 console.log(`Oi, testando o console.log`); //String
 
 console.log(5, 16.872, 11); // Numbers
-console.log(5, 16.872, 11, "String aqui"); // Numbers
+console.log(5, 16.872, 11, "String aqui"); // String e Numbers
