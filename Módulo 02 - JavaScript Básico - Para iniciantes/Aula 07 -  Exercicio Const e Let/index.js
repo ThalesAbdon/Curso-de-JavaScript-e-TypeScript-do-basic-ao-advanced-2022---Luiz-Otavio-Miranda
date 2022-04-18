214d393d9cbe3af74b589b78aa8e3097df20fd6f
Thales <thales.sousufpi@gmail.com>
2022-04-18 20:29:17 -0300

    Calculando IMC

---
+++ Módulo 02 - JavaScript Básico - Para iniciantes/Aula 07 -  Exercicio Const e Let/index.js
@@ -0,0 +1,10 @@
+/*Calcular IMC*/
+const nome = "João";
+const sobrenome = "Silva";
+const idade = 25;
+const peso = 84;
+const altura = 1.80;
+let imc = peso/(altura*altura);
+let anoNascimento = 2022 - 25;
+
+console.log(nome,sobrenome,"tem",idade,"pesa:",peso,"kg","possui",altura,"de altura","e seu IMC é de: ",imc,nome,"nasceu em:",anoNascimento)
