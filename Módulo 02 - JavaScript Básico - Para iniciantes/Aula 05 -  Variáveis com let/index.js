7a82cfed94a33a820f4c064af2e0a96879214ec2
Thales <thales.sousufpi@gmail.com>
2022-04-17 21:05:39 -0300

    Adicionando comentários

--- Módulo 02 - JavaScript Básico - Para iniciantes/Aula 05 -  Variáveis com let/index.js
+++ Módulo 02 - JavaScript Básico - Para iniciantes/Aula 05 -  Variáveis com let/index.js
@@ -1,5 +1,12 @@
+//Não podemos criar variáveis com palavras reservadas da linguagem
+//Não podemos redeclarar variáveis com let
+//Não podemos começar o nome de uma variável com um número
+//Não podem conter espaços ou traços
+//NÃO UTILIZE VAR, UTILIZE LET
+//Utilizamos camelCase
+//As variáveis são Case-sensitive
+//As variáveis precisam ter nomes significativos
 let nome = 'Maria'
-
 console.log(nome,'nasceu em 2000');
 console.log('Em 2020' ,nome,'conheceu João');
 console.log('Então João casou com',nome,'em 2026');
