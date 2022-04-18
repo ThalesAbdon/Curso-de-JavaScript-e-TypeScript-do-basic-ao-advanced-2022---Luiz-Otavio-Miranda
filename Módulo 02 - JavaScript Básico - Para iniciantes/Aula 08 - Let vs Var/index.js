0c0c798cd7f821b82925a76bd323fca497c3f151
Thales <thales.sousufpi@gmail.com>
2022-04-18 20:45:33 -0300

    Estudando Let vs Var e Tipos de dados primitivos

---
+++ Módulo 02 - JavaScript Básico - Para iniciantes/Aula 08 - Let vs Var/index.js
@@ -0,0 +1,7 @@
+var nomeVar ='João';
+var nomeVar = 'Lucas';
+console.log(nomeVar);
+let nomeLet = 'Bianca'
+//let nomeLet = 'Maria' isso não pode pois o Let só permite a troca sem usar let novamente
+nomeLet = 'Maria'
+console.log(nomeLet);
---
+++ Módulo 02 - JavaScript Básico - Para iniciantes/Aula 09 - Tipos de dados primitivos/index.js
@@ -0,0 +1,8 @@
+const nome1 = 'Lucas'; //string
+const nome2 = "Lucas"; //string
+const nome3 = `Lucas`; //string
+const num1 = 10; //number
+const num2 = 10.21321321; //number
+let nomeAluno;//undefined -> não aponta para nenhum endereço de memória
+const sobrenomeAluno = null; //Nulo -> não aponta  aponta par nenhum endereço de memória, porém EU(programador) que indiquei para ser Nulo
+const boolean = true; // Boolean = true or false
