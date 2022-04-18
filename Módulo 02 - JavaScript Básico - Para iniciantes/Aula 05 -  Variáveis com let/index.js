e81fffad298c870aebedb321886c259467710c73
Thales <thales.sousufpi@gmail.com>
2022-04-17 21:03:43 -0300

    Aprendendo Variáveis com let

---
+++ Módulo 02 - JavaScript Básico - Para iniciantes/Aula 05 -  Variáveis com let/index.js
@@ -0,0 +1,8 @@
+let nome = 'Maria'
+
+console.log(nome,'nasceu em 2000');
+console.log('Em 2020' ,nome,'conheceu João');
+console.log('Então João casou com',nome,'em 2026');
+console.log('Em 2029' ,nome, 'ficou grávida');
+console.log( nome,'deu a luz em 2030');
+console.log('A filha de João e',nome,'se chama joana!');
