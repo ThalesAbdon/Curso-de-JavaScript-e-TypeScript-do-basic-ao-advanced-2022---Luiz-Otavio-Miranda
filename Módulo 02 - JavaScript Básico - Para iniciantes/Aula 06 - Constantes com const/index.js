e65cf781cc5192e0a5dc546e825cc82cb688ce42
Thales <thales.sousufpi@gmail.com>
2022-04-17 21:20:03 -0300

    Aprendendo Constantes com const e fazendo as 4 operações básicas da matemática

---
+++ Módulo 02 - JavaScript Básico - Para iniciantes/Aula 06 - Constantes com const/index.js
@@ -0,0 +1,19 @@
+//Não podemos criar constantes com palavras reservadas da linguagem
+//Não podemos modificar o valor de uma constante
+//Não podemos começar o nome de uma constantes com um número
+//Não podem conter espaços ou traços
+//NÃO UTILIZE VAR, UTILIZE LET
+//Utilizamos camelCase
+//As constantes são Case-sensitive
+//As constantes precisam ter nomes significativos
+
+const primeiroNumero = 30;
+const segundoNumero = 10;
+const sum = primeiroNumero + segundoNumero;
+const subtraction = primeiroNumero - segundoNumero;
+const multiplication = primeiroNumero * segundoNumero;
+const division = primeiroNumero / segundoNumero;
+console.log(sum)
+console.log(subtraction)
+console.log(multiplication)
+console.log(division)
