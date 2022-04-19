275319939072f6695d98d881ecfef09d8f69bfd4
Thales <thales.sousufpi@gmail.com>
2022-04-19 14:10:41 -0300

    Exercício de soma com input do alert

---
+++ Módulo 02 - JavaScript Básico - Para iniciantes/Aula 12 - Exercicio com alert e operadores aritméticos/js/scripts.js
@@ -0,0 +1,6 @@
+let numero1 = prompt("Digite o valor do primeiro número");
+numero1 = Number(numero1);
+let numero2 = prompt("Digite o valor do segundo número");
+numero2 = Number(numero2);
+const resultado = numero1 + numero2;
+alert(`O Resultado da soma é: ${resultado}`)
