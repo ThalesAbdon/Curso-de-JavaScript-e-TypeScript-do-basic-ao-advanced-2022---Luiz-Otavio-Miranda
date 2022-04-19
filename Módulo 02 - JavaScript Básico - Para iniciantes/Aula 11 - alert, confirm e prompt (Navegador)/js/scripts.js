e079e6dd76d6da5a35d3a1ba04912e35dd7406e5
Thales <thales.sousufpi@gmail.com>
2022-04-19 14:05:31 -0300

    Somando 2 números recebidos do input do prompt

---
+++ Módulo 02 - JavaScript Básico - Para iniciantes/Aula 11 - alert, confirm e prompt (Navegador)/js/scripts.js
@@ -0,0 +1,8 @@
+alert('Uma mensagem aqui!');
+confirm("deseja apagar?");
+prompt("Digite seu nome");
+let numero1 = prompt("Digite um numero");
+numero1 = Number(numero1);
+let numero2 = prompt("Digite um outro numero");
+numero2 = Number(numero2)
+alert(`O Resultado da soma é: ${numero1 + numero2}`)
