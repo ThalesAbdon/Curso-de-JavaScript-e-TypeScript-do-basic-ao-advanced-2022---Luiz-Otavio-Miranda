810498680bc3157adf7b0d9a3e28da3b8ef73c51
Thales <thales.sousufpi@gmail.com>
2022-04-27 18:03:06 -0300

    Estudando if, else if e else

---
+++ Módulo 03 - JavaScript - Lógica De Programação/Aula 04 - if, else if e else Parte 1/index.js
@@ -0,0 +1,18 @@
+const hora = 10;
+
+if( hora >= 0 && hora <= 11){
+    console.log('Bom Dia');
+}else if(hora >= 12 && hora <= 17){
+    console.log('Boa Tarde');
+}else if(hora >= 18 && hora <= 23){
+    console.log('Boa Noite');
+}else{
+    console.log('Olá')
+}
+
+const tenhoMoney = true;
+if(tenhoMoney){
+    console.log('Vou sair de casa');
+}else{
+    console.log('Não vou sair de casa')
+}
