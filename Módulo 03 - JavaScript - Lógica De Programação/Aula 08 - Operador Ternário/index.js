79da46b0b275c10fd077a985b9c73bd328580ea8
Thales <thales.sousufpi@gmail.com>
2022-04-29 16:32:19 -0300

    Estudando operador ternário em Javascript

---
+++ Módulo 03 - JavaScript - Lógica De Programação/Aula 08 - Operador Ternário/index.js
@@ -0,0 +1,4 @@
+const score = 1000;                
+                                //true                //false
+const vipUser = score >= 1000 ? 'Sim, ele é vip' : 'Não, ele não é vip!'
+console.log(vipUser)
