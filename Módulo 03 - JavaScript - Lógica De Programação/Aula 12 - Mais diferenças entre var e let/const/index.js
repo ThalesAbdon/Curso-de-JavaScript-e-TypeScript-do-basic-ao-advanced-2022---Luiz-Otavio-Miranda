e5cbdc858d100e64f45c87a863054c9b4653023e
Thales <thales.sousufpi@gmail.com>
2022-05-02 15:17:19 -0300

    Estudando mais diferenças entre var/const/let

---
+++ Módulo 03 - JavaScript - Lógica De Programação/Aula 12 - Mais diferenças entre var e let/const/index.js
@@ -0,0 +1,14 @@
+let nome = 'Thales';
+var nome2 = 'Thales';
+
+// não podemos redeclarar a variavel let
+// let nome = 'Julia';
+//podemos redeclarar a variavel var
+
+var nome2 = 'Luiza'
+
+if(true){
+    let nome = 'Maria';
+    //let tem escopo de bloco!
+    console.log(nome,nome2)
+}
