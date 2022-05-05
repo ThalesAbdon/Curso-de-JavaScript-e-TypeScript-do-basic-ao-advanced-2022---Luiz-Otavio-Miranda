6121d5ca05293285e6d42a78d57d63875494d892
Thales <thales.sousufpi@gmail.com>
2022-05-05 19:13:19 -0300

    Revisando a estrutra de repetição For

---
+++ Módulo 03 - JavaScript - Lógica De Programação/Aula 15 - Estrutura de Repetição For/index.js
@@ -0,0 +1,11 @@
+
+for(let n = 0; n < 10; n++){
+    console.log("Isso vai se repetir " + (n+1) + " vezes");
+
+}
+
+const pokemons = ['Squirtle','Charmander','Bulbasaur','Pikachu','Togepi'];
+
+for(let i = 0; i < pokemons.length; i++) {
+    console.log(pokemons[i])
+}
