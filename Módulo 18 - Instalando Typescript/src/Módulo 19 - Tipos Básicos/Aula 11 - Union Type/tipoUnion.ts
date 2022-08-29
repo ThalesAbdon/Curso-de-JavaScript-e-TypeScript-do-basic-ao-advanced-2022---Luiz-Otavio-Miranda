20f0f094f6a732682cb9ea114cc638b35b858419
Thales Sousa <thales.sousufpi@gmail.com>
2022-08-29 16:21:48 -0300

    Estudando type Union

---
+++ Módulo 18 - Instalando Typescript/src/Módulo 19 - Tipos Básicos/Aula 11 - Union Type/tipoUnion.ts
@@ -0,0 +1,8 @@
+export function addOrConcat(a: number | string, b: number | string) {
+  if (typeof a === 'number' && typeof b === 'number') return a + b;
+  // if (typeof a === 'string' && typeof b === 'string') return a + b;
+  return `${a}${b}`;
+}
+
+console.log(addOrConcat(10, 20));
+console.log(addOrConcat('10', '20'));
