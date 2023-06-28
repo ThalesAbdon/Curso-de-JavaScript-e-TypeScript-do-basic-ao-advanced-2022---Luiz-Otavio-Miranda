452b7ff6037eb3b2573b3596121e6e2b009eb2a0
ThalesAbdon <thales.sousufpi@gmail.com>
2023-06-28 09:10:37 -0300

    Estudando Object defineProperty e defineProperties

---
+++ Módulo 06 - JavaScript Objetos e prototypes (Avançado)/Aula 02 - Object.defineProperty() e Object.defineProperties()/index.js
@@ -0,0 +1,21 @@
+function Produto(name, price, stock) {
+  Object.defineProperties(this, {
+    name: { enumerable: true, value: name, writable: true, configurable: true },
+    price: {
+      enumerable: true,
+      value: name,
+      writable: true,
+      configurable: true,
+    },
+  });
+
+  Object.defineProperty(this, "stock", {
+    enumerable: false, //mostra a chave
+    value: stock, // valor
+    writable: false, // pode alterar o valor
+    configurable: false, // configurar
+  });
+}
+
+const produto1 = new Produto("Camisa", 299, 7);
+console.log(produto1);
