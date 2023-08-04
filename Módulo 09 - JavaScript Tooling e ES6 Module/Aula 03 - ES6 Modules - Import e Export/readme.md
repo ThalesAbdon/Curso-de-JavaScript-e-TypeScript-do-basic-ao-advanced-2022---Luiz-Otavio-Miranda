680ef0b87b2716fa62a64b1e4fa7fcfeea9c025c
ThalesAbdon <thales.sousufpi@gmail.com>
2023-08-04 08:29:21 -0300

    Estudando import e export

---
+++ Módulo 09 - JavaScript Tooling e ES6 Module/Aula 03 - ES6 Modules - Import e Export/readme.md
@@ -0,0 +1,3 @@
+npm init -y
+
+npm i --save-dev @babel/preset-env @babel/core @babel/cli babel-loader webpack webpack-cli regenerator-runtime core-js@2
---
+++ Módulo 09 - JavaScript Tooling e ES6 Module/Aula 03 - ES6 Modules - Import e Export/src/modulo1.js
@@ -0,0 +1,7 @@
+export const nome = "Thales";
+export const sobrenome = "Abdon";
+export const idade = 30;
+
+export function soma(x, y) {
+  return x + y;
+}
