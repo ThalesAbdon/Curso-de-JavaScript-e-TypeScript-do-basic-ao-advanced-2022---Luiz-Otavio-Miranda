4a43009ae37c4849abac3d293001d9c280bcad7f
ThalesAbdon <thales.sousufpi@gmail.com>
2023-09-08 15:36:40 -0300

    feat: create seeds

---
+++ Módulo 13 - API REST/API_REST/src/database/seeds/20230908183238-criar-usuarios.js
@@ -0,0 +1,16 @@
+/** @type {import('sequelize-cli').Migration} */
+const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert('users', [{
+      nome: 'Thales',
+      email: 'thales@hotmail.com',
+      password_hash: await bcryptjs.hash('123456', 8),
+      created_at: new Date(),
+      updated_at: new Date(),
+    }], {});
+  },
+
+  down: () => {},
+};
